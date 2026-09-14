560cc4b3c580f97c66368c9da2adfe628158031f
Karthikeyan.T <tamil16rkd@gmail.com>
2026-09-14 22:06:03 +0530

    Create task 13.js

---
+++ task 13.js
@@ -0,0 +1,27 @@
+var name = "karthi";
+let age = 18;
+let isstudent = true;
+const DOB = "27/11/2007";
+
+let sum = age + 5;
+let diff = age - 5;
+let prod = age*2;
+let div = age/5;
+let remainder = age%5;
+
+let issame = (age==18);
+let isequal = (age==="18");
+
+console.log(name);
+console.log(age);
+console.log(isstudent);
+console.log(DOB);
+
+console.log(sum);
+console.log(diff);
+console.log(prod);
+console.log(div);
+console.log(remainder);
+
+console.log(issame);
+console.log(isequal);
